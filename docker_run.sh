3f75b015fecfd4e8977338f2c697e2810036c6f3
gcwl <44855893+gcwl@users.noreply.github.com>
2019-05-24 10:17:48 +0800

    refactored run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-# get free ports from host
+GREP=/usr/bin/grep
+
+# get free/unprivileged ports (20000-65000)
 get_port () {
-  (netstat -4 --all --numeric --tcp --udp | awk '{printf "%s\n%s\n", $4, $4}' | grep -oE '[0-9]+$'; seq ${2:-20000} ${3:-65000}) \
-    | sort -n \
-    | uniq -u \
-    | shuf \
-    | head -n ${1:-1}
+  (netstat -4 --all --numeric --tcp --udp \
+    | awk '{printf "%s\n%s\n", $4, $4}' \
+    | $GREP -oE '[0-9]+$'; \
+   seq ${2:-20000} ${3:-65000}) \
+      | sort -n \
+      | uniq -u \
+      | shuf \
+      | head -n ${1:-1}
 }
 
 # run nvidia-docker
@@ -23,16 +28,16 @@ Binding ports [host:container]
 EOF
 
   nvidia-docker run \
-      -it --rm \
-      --ipc="host" \
-      -p 127.0.0.1:${PORTS[0]}:8888 \
-      -p 127.0.0.1:${PORTS[1]}:8889 \
-      -p 127.0.0.1:${PORTS[2]}:8890 \
-      -v $(pwd):/workspace \
-      -v $HOME/.torch:/root/.torch \
-      -v $HOME/.pytorch_pretrained_bert:/root/.pytorch_pretrained_bert \
-      $IMAGE \
-      bash
+    -it --rm \
+    --ipc="host" \
+    -p 127.0.0.1:${PORTS[0]}:8888 \
+    -p 127.0.0.1:${PORTS[1]}:8889 \
+    -p 127.0.0.1:${PORTS[2]}:8890 \
+    -v $(pwd):/workspace \
+    -v $HOME/.torch:/root/.torch \
+    -v $HOME/.pytorch_pretrained_bert:/root/.pytorch_pretrained_bert \
+    $IMAGE \
+    bash
 
   ## shared memory segment size
   # https://docs.nvidia.com/deeplearning/frameworks/user-guide/index.html#setincshmem
@@ -42,8 +47,8 @@ EOF
 }
 
 # select docker image via fzf
-IFS=$'\n' match=$(docker images | grep "^$(whoami)" | awk '$2=="latest" {print $1}' | fzf)
+# only show images with tag=latest for selection
+IFS=$'\n' MATCH=$(docker images | $GREP "^$(whoami)" | awk '$2=="latest" {print $1}' | fzf)
 
 # run target image
-[ -n "$match" ] && run $match || echo "No docker image selected."
-
+[ -n "$MATCH" ] && run $MATCH || echo "No docker image selected."
